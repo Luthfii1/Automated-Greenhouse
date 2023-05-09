151a1254203005ea85ff2506c96138235f526e15
Luthfii1 <luthfi.misbachul@ui.ac.id>
2023-05-09 10:53:18 +0700

    Rangkaian finished, code masih belum bisa integrasi di servo

--- code_finpro.ino
+++ code_finpro.ino
@@ -1,14 +1,9 @@
-int pinLED = 12;
-
 void setup() {
   // put your setup code here, to run once:
-pinMode(pinLED, OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(pinLED, HIGH);
-delay(1000);
-digitalWrite(pinLED, LOW);
-delay(1000);
+
 }
